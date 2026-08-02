a848d1d0a7182ffbda3aa7772699c64103bbf55c
jayasuryaa05-sys <jayasuryaa022@gmail.com>
2026-08-02 20:23:40 +0530

    feat(router):add react router pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Explore from "./pages/Explore";
+import Profile from "./pages/Profile";
+import ComicDetails from "./pages/ComicDetails";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-5xl font-bold text-green-400">
-        ComicVerse AI
-      </h1>
-    </div>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/explore" element={<Explore />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/comic/:id" element={<ComicDetails />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
   );
 }
 
